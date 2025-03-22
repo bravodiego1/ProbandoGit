55045284431c8d355b3128bd922af890692ec7f8
bravodiego1 <dibravo@frba.utn.edu.ar>
2025-03-22 11:31:44 -0300

    Agrego cuenta random

---
+++ sumando.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int numero = 10;
+    int resultado = numero * 50;
+
+    printf("El resultado de la operacion es: %d",resultado);
+    
+    
+    return 0;
+}
